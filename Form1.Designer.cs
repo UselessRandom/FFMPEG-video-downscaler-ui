da41d40e89624f72e2ff656261eee23b7a2befc1
UselessRandom <88690922+UselessRandom@users.noreply.github.com>
2022-03-06 20:41:29 -0700

    Upload files

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace FFMPEG_video_downscaler_ui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.w = new System.Windows.Forms.TextBox();
+            this.h = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.downscaleBtn = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(698, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(716, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Width";
+            // 
+            // w
+            // 
+            this.w.Location = new System.Drawing.Point(371, 121);
+            this.w.Name = "w";
+            this.w.Size = new System.Drawing.Size(100, 20);
+            this.w.TabIndex = 3;
+            // 
+            // h
+            // 
+            this.h.Location = new System.Drawing.Point(371, 147);
+            this.h.Name = "h";
+            this.h.Size = new System.Drawing.Size(100, 20);
+            this.h.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Height";
+            // 
+            // downscaleBtn
+            // 
+            this.downscaleBtn.Location = new System.Drawing.Point(716, 253);
+            this.downscaleBtn.Name = "downscaleBtn";
+            this.downscaleBtn.Size = new System.Drawing.Size(75, 23);
+            this.downscaleBtn.TabIndex = 6;
+            this.downscaleBtn.Text = "Downscale";
+            this.downscaleBtn.UseVisualStyleBackColor = true;
+            this.downscaleBtn.Click += new System.EventHandler(this.downscaleBtn_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 256);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(698, 20);
+            this.outputBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 288);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.downscaleBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.h);
+            this.Controls.Add(this.w);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Down scaler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox w;
+        private System.Windows.Forms.TextBox h;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button downscaleBtn;
+        private System.Windows.Forms.TextBox outputBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFMPEG_video_downscaler_ui
+{
+    public partial class Form1 : Form
+    {   
+        private OpenFileDialog ofd;
+        private SaveFileDialog sfd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            ofd.Filter = "MP4 (*.mp4)|*.mp4|MOV (*.mov)|*.mov|AVI (*.avi)|*.avi|FLV (*.flv)|*.flv|MKV (*.mkv)|*.mkv";
+            sfd = new SaveFileDialog();
+            sfd.Filter = "MP4 (*.mp4)|*.mp4|MOV (*.mov)|*.mov|AVI (*.avi)|*.avi|FLV (*.flv)|*.flv|MKV (*.mkv)|*.mkv";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ofd.ShowDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.FileInfo ofdInfo = new System.IO.FileInfo(ofd.FileName);
+                if (ofdInfo.ToString().Any(Char.IsWhiteSpace) == true)
+                {
+                    MessageBox.Show("File name cannot have spaces", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    this.textBox1.Text = ofdInfo.ToString();
+                }
+            }
+        }
+
+        private void downscaleBtn_Click(object sender, EventArgs e)
+        {
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = sfd.FileName;
+                if (filePath.Any(Char.IsWhiteSpace) == true)
+                {
+                    MessageBox.Show("Output file name cannot have spaces", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    outputBox.Text = filePath;
+                    string ffmpegCMD = $"/C ffmpeg -i {textBox1.Text} -vf scale={w.Text}:{h.Text} {outputBox.Text}";
+                    System.Diagnostics.Process.Start("CMD.exe", ffmpegCMD);
+                    Console.WriteLine(ffmpegCMD);
+                }
+            }
+            
+        }
+    }
+}
